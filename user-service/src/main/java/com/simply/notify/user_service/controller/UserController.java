2c6bb464dbbc246492cf8ef01595c3ecac03a961
Akhil Bhadauria <akhilbhadauria1996@gmail.com>
2025-12-10 20:06:09 +0530

    Added UserController

--- user-service/src/main/java/com/simply/notify/user_service/controller/UserController.java
+++ user-service/src/main/java/com/simply/notify/user_service/controller/UserController.java
@@ -1,19 +1,42 @@
 package com.simply.notify.user_service.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.simply.notify.user_service.entity.User;
+import com.simply.notify.user_service.repo.UserRepo;
 
-@RestController(value = "/user")
+@RestController
+@RequestMapping(value = "/user")
 public class UserController {
 	
-	@GetMapping(value = "/{id}")
-	public User getUser(@RequestParam(value = "id") Long id) {
-		User user = new User();
+	@Autowired
+	UserRepo userRepo;
+	
+	@PostMapping
+	public User createUser(@RequestBody User user) {
+		user = userRepo.save(user);
 		return user;
+	}
+	
+	@GetMapping(value = "/{id}")
+	public User getUser(@PathVariable(value = "id") Long id) {
+		User user = userRepo.findById(id).get();
+		return user;
+	}
+	
+	@GetMapping
+	public List<User> getUserList(){
+		List<User> userList = userRepo.findAll();
+		return userList;
 	}
 
 }
