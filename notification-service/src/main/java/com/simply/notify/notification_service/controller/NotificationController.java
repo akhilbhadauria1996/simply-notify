562c544556d8521acd6ca967d810d131d8e3a580
Akhil Bhadauria <akhilbhadauria1996@gmail.com>
2025-12-23 20:38:15 +0530

    Added more fields in UI page

--- notification-service/src/main/java/com/simply/notify/notification_service/controller/NotificationController.java
+++ notification-service/src/main/java/com/simply/notify/notification_service/controller/NotificationController.java
@@ -2,6 +2,7 @@ package com.simply.notify.notification_service.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import com.simply.notify.notification_service.service.NotificationService;
 
 @RestController
 @RequestMapping(value = "/notification")
+@CrossOrigin(origins = "http://localhost:5173")
 public class NotificationController {
 	
 	@Autowired
--- simply-notify-ui/src/components/CreateNotification.jsx
+++ simply-notify-ui/src/components/CreateNotification.jsx
@@ -1,28 +1,79 @@
-import { useState } from "react";
+import { use, useState } from "react";
 
-function CreateNotification(){
+function CreateNotification() {
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [body, setBody] = useState("");
+  const [userId, setUserId] = useState("");
+  const [loading, setLoading] = useState("");
+  const [successMsg, setSuccessMsg] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
 
-    const [email, setEmail] = useState("");
-    const [subject, setSubject] = useState("");
-    const [body, setBody] = useState("");
+  const handleSubmit = async () => {
+    console.log({ email, subject, body });
 
-    const handleSubmit = () => {
-        // const pay
+    const payload = {
+      userId: 1,
+      type: "EMAIL",
+      to: email,
+      subject: subject,
+      body: body,
+    };
+
+    const url = "http://localhost:8082/notification";
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to create notification");
+      }
+
+      const data = await response.json();
+      console.log(data);
+
+      alert("Notification created !");
+    } catch (error) {
+      console.error(error);
+      alert("Failed to create Notification");
     }
+  };
 
-    return(
-        <div>
-            <h2>Create Notification</h2>
-            <div>
-                <label>Email:</label>
-                <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} ></input>
-                <label>Subject:</label>
-                <input type="text" value={subject} onChange={(e) => setSubject(e.target.value)} ></input>
-                <label>Body:</label>
-                <input type="text" value={body} onChange={(e) => setBody(e.target.value)} ></input>
-            </div>
-            <button onClick={(e) => handleSubmit} >Submit</button>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Create Notification</h2>
+      <div>
+        <label>Email:</label>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+      </div>
+      <div>
+        <label>Subject:</label>
+        <input
+          type="text"
+          value={subject}
+          onChange={(e) => setSubject(e.target.value)}
+        ></input>
+      </div>
+      <div>
+        <label>Body:</label>
+        <textarea
+          type="text"
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+      </div>
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
 }
-export default CreateNotification
+export default CreateNotification;
